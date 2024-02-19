1cd3ad989a14be711b8f8d69dacf3b924c960f41
Mehran <mehranbhat2001@gmail.com>
2024-02-19 12:36:25 +0530

    added navigation links

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import Home from "./Components/Home";
 import Contact from "./Components/Contact";
 import About from "./Components/About";
 import Blog from "./Components/Blog";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/About" element={<About />} />
-      <Route path="/blog" element={<Blog />} />
-    </Routes>
+    <>
+      <h2
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        My React-Router-Dom
+      </h2>
+      <header>
+        <Navbar />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/blog">Blog</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
